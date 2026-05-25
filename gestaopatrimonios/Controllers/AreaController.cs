ba995157c950719c3f088c15d0c0541306ec2d2d
Mayara <mayara.s.almeida8@aluno.senai.br>
2026-05-25 11:00:11 -0300

    :tada: Preparação do ambiente para início do front do projeto e validação da api

---
+++ gestaopatrimonios/Controllers/AreaController.cs
@@ -0,0 +1,76 @@
+﻿using GestaoPatrimonios.Applications.Services;
+using GestaoPatrimonios.DTOs.AreaDto;
+using GestaoPatrimonios.Exceptions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestaoPatrimonios.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AreaController : ControllerBase
+    {
+        private readonly AreaService _service;
+
+        public AreaController(AreaService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public ActionResult<List<ListarAreaDto>> Listar()
+        {
+            List<ListarAreaDto> areas = _service.Listar();
+
+            return Ok(areas);
+        }
+
+        [HttpGet("{id}")]
+        [Authorize]
+        public ActionResult<ListarAreaDto> BuscarPorId(Guid id)
+        {
+            try
+            {
+                ListarAreaDto area = _service.BuscarPorId(id);
+
+                return Ok(area);
+            }
+            catch (DomainException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Coordenador")]
+        public ActionResult Adicionar(CriarAreaDto dto)
+        {
+            try
+            {
+                _service.Adicionar(dto);
+                return Created();
+            }
+            catch (DomainException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        [Authorize(Roles = "Coordenador")]
+        public ActionResult Atualizar(Guid id, CriarAreaDto dto)
+        {
+            try
+            {
+                _service.Atualizar(id, dto);
+                return NoContent();
+            }
+            catch(DomainException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ gestaopatrimonios/Controllers/BairroController.cs
@@ -0,0 +1,72 @@
+﻿using GestaoPatrimonios.Applications.Services;
+using GestaoPatrimonios.DTOs.Bairro;
+using GestaoPatrimonios.Exceptions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestaoPatrimonios.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BairroController : ControllerBase
+    {
+        private readonly BairroService _service;
+
+        public BairroController(BairroService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public ActionResult<List<ListarBairroDto>> Listar()
+        {
+            return Ok(_service.Listar());
+        }
+
+        [HttpGet("{id}")]
+        [Authorize]
+        public ActionResult<ListarBairroDto> BuscarPorId(Guid id)
+        {
+            try
+            {
+                return Ok(_service.BuscarPorId(id));
+            }
+            catch (DomainException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Coordenador")]
+        public ActionResult Adicionar(CriarBairroDto dto)
+        {
+            try
+            {
+                _service.Adicionar(dto);
+                return Created();
+            }
+            catch (DomainException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        [Authorize(Roles = "Coordenador")]
+        public ActionResult Atualizar(Guid id, CriarBairroDto dto)
+        {
+            try
+            {
+                _service.Atualizar(id, dto);
+                return NoContent();
+            }
+            catch (DomainException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
